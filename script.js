10c8d9c03c0ad10ef826e2afecaf60702ccc0c0d
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-08-05 19:58:29 -0400

    Feito as funções

---
+++ script.js
@@ -0,0 +1,72 @@
+let inputTarefa = document.querySelector("#nomeTarefa");
+let inputData = document.querySelector("#data");
+let conteudoTabela = document.querySelector("#conteudoTabela");
+let btnAddLivro = document.querySelector("#btnAddLivro");
+let mensagem = document.querySelector("#mensagem");
+
+let listaTarefas = [];
+
+
+function getTarefas(){
+    listaTarefas.JSON.parse(localStorage.getItem
+        ("listaTarefas")) || []
+}
+
+function setTarefas(){
+    localStorage.setItem("listaTarefas", JSON.stringify
+        (listaTarefas))
+}
+
+function addTarefa(nomeTarefa, data){
+    let tarefa = {
+        tarefa: nomeTarefa,
+        data: data
+    }
+
+    listaTarefas.push(tarefa)
+    setTarefas()
+    mostrarMensagem("Tarefa adicionada")
+}
+
+btnAddLivro.addEventListener('click', function(e){
+    e.preventDefault()
+    addTarefa(inputTarefa.value, inputData.value);
+    console.table(listaTarefas);
+    limparFormulario()
+    renderizarTabela()
+})
+
+function limparFormulario(){
+    inputTarefa.value = ""
+    inputData.value = ""
+    inputTarefa.focus()
+}
+
+function renderizarTabela(){
+    conteudoTabela.innerHTML = `
+    <table class="tabela">
+        <tr>
+            <th>Tarefa</th>
+            <th>Data de conclusão</th>
+            <th>Ação</th>
+        </tr>
+        ${listaTarefas.map(tarefa =>
+            `<tr>
+                <td>${tarefa.nomeTarefa}</td>
+                <td>${tarefa.data}</td>
+            <tr/>
+            `
+        ).join('')}
+    </table>
+    `
+}
+
+function mostrarMensagem(texto){
+    mensagem.style.display = 'block'
+    mensagem.innerHTML = texto
+
+    setTimeout(function(){
+        mensagem.style.display = 'none'
+        mensagem.innerHTML = ''
+    }, 3000)
+}
